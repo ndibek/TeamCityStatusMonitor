a528f0be082e8948b11b4166b81f75bb840f0090
jholdstock <jam.jar69@gmail.com>
2014-11-02 11:58:37 +0000

    dont set bg colour using javascript

--- js/replacement-page.js
+++ js/replacement-page.js
@@ -3,7 +3,7 @@ function ReplacementPage(hideCursor) {
 	this.hideCursor = hideCursor;
 
   this.renderSuccess = function(successMessage) {
-		var boxElement = $("<div>").addClass("tsm_box");
+		var boxElement = $("<div>").addClass("tsm_box").addClass("tsm_success");
 
 		if (this.hideCursor) {
 			boxElement.css("cursor", "none");
@@ -30,20 +30,20 @@ function ReplacementPage(hideCursor) {
 			}
 
 			if (build.hasFailed()) {
+				boxElement.addClass("tsm_failure");
 				var heading = $("<div>").text(build.getName()).addClass("tsm_configurationName");
 				var number  = $("<div>").text(build.getNumber()).addClass("tsm_buildNumber");
 				var date    = $("<div>").text("Failed on " + build.getDate()).addClass("tsm_date");
 				var elapsed = $("<div>").text(build.getElapsedTime()).addClass("tsm_elapsed");
 
-				boxElement.css("background-color", "red");
 				boxElement.append(heading).append(number).append(date).append(elapsed);
 				boxElement.appendTo($("body"));	
 			}
 			else {
+				boxElement.addClass("tsm_notrun");
 				var heading = $("<div>").text(build.getName()).addClass("tsm_configurationName");
 				var message = $("<div>").html("Build has never run").addClass("tsm_date");
 
-				boxElement.css("background-color", "orange");
 				boxElement.append(heading).append(message);;
 				boxElement.appendTo($("body"));
 			}
