ee9736fc7bf082fbb824ee28ac30c0af7e927169
jholdstock <jam.jar69@gmail.com>
2014-11-29 11:00:54 +0000

    give css classes less specific names

--- js/replacement-page.js
+++ js/replacement-page.js
@@ -4,9 +4,9 @@ function ReplacementPage() {
 
   this.renderSuccess = function(successMessage) {
 		this.appendToBody(this.wrapper);
-		var bgElement = $("<div>").addClass("tsm_success");
+		var bgElement = $("<div>").addClass("tsm_green");
 
-		var heading = $("<div>").addClass("tsm_success_text").html(successMessage);
+		var heading = $("<div>").addClass("tsm_centre").html(successMessage);
 
 		bgElement.append(heading);
 
@@ -27,10 +27,10 @@ function ReplacementPage() {
 
 			var dateString = "Failed on " + build.getDate() + "<br /><br/>" + build.getElapsedTime();
 
-			bgElement  = $("<div>").addClass("tsm_failure");
-			var heading = $("<div>").addClass("tsm_configurationName").html(build.getName());
-			var number  = $("<div>").addClass("tsm_buildNumber").html(build.getNumber());
-			var date    = $("<div>").addClass("tsm_date").html(dateString);
+			bgElement  = $("<div>").addClass("tsm_red");
+			var heading = $("<div>").addClass("tsm_topLeft").html(build.getName());
+			var number  = $("<div>").addClass("tsm_bottomLeft").html(build.getNumber());
+			var date    = $("<div>").addClass("tsm_topRight").html(dateString);
 
 			bgElement.append(heading).append(number).append(date);
 
@@ -40,9 +40,9 @@ function ReplacementPage() {
 		$.each(notRunBuilds, function(index, build) {
 			var bgElement;
 
-			bgElement = $("<div>").addClass("tsm_notrun");
-			var heading = $("<div>").addClass("tsm_configurationName").html(build.getName());
-			var message = $("<div>").addClass("tsm_date").html("Build has never run");
+			bgElement = $("<div>").addClass("tsm_orange");
+			var heading = $("<div>").addClass("tsm_topLeft").html(build.getName());
+			var message = $("<div>").addClass("tsm_topRight").html("Build has never run");
 
 			bgElement.append(heading).append(message);;
 
